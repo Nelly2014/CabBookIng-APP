c7e2e1a5e598710cd054ad250d16b700a4f94ed3
Neliswa Sililo <neliswasililo2@gmail.com>
2026-04-29 10:35:23 +0200

    Update ResponseMessage.java

--- src/main/java/com/example/cabbooking/entity/ResponseMessage.java
+++ src/main/java/com/example/cabbooking/entity/ResponseMessage.java
@@ -1,1 +1,27 @@
 
+package com.example.cabbooking.entity;
+
+public class ResponseMessage {
+
+    private int code;
+    private String message;
+    private Object data;
+
+    public ResponseMessage(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public ResponseMessage(int code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public int getCode()                    { return code; }
+    public void setCode(int code)           { this.code = code; }
+    public String getMessage()              { return message; }
+    public void setMessage(String message)  { this.message = message; }
+    public Object getData()                 { return data; }
+    public void setData(Object data)        { this.data = data; }
+}
